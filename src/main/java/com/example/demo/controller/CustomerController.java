7751e665606c52f32249c3fc7df235b7ac6a2ce2
Matvey <caramel3355for@gmail.com>
2024-06-13 21:21:43 +0300

    **** java and **** idea

---
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+}
